cc7ff80ff07acc0b8b9371864574b9dca2ff5b56
NotHalls <notharshith74@gmail.com>
2024-11-18 21:20:57 +0000

    we can now set variables and other stuff in c

--- src/kernel/Kernel.c
+++ src/kernel/Kernel.c
@@ -2,9 +2,13 @@
 
 
 void kernel_main()
-{ 
-    const char* msg = "HI";
-    Log(msg);
+{
+    const char* startMessage = "HI, I'm Ginger! \nI'm A Cat";
+
+    ClearScreen();
+
+    SetLogColor(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+    Log(startMessage);
 
     while(1) {}
 }
--- src/kernel/core/Logger.c
+++ src/kernel/core/Logger.c
@@ -1,17 +1,101 @@
 #include "Logger.h"
 
-// VGA Text Mode starting address
-#define VGA_TEXT (char*)0xB8000
+// some consts that we cant control
+const static size_t NUM_COLS = 80;
+const static size_t NUM_ROWS = 25;
+
+struct VGAChar
+{
+    uint8_t character;
+    uint8_t color;
+};
+
+// starting from the point 0xb8000 we are creating a buffer of
+// memory of chars
+struct VGAChar* buffer = (struct VGAChar*) 0xb8000;
+
+size_t row = 0;
+size_t col = 0;
+// the color we will be using. white for fg, black for bg
+uint8_t currentColor = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
+
+
+// private functions //
+void clearRow(size_t pRow)
+{
+    struct VGAChar empty = (struct VGAChar) {
+        character: ' ',
+        color: currentColor
+    };
+    
+    for(size_t c = 0; c < NUM_COLS; c++)
+        buffer[c + NUM_COLS * pRow] = empty;
+}
+
+void logNewLine()
+{
+    col = 0;
+
+    if(row < NUM_ROWS - 1)
+    {
+        row++;
+        return;
+    }
+
+    // we move all the rows up by 1 if the whole screen is filled
+    for(size_t r = 1; r < NUM_ROWS; r++)
+    {
+        for(size_t c = 0; c < NUM_COLS; c++)
+        {
+            struct VGAChar character = buffer[c + NUM_COLS * r];
+            buffer[c + NUM_COLS * (r - 1)] = character;
+        }
+    }
+
+    clearRow(NUM_ROWS - 1);
+}
+
+
+// public functions //
+void ClearScreen()
+{
+    for(size_t i = 0; i < NUM_ROWS; i++)
+        clearRow(i);
+}
+
+void LogChar(char c)
+{
+    if(c == '\n')
+    {
+        logNewLine();
+        return;
+    }
+
+    if(col > NUM_COLS)
+    {
+        logNewLine();
+    }
+
+    buffer[col + NUM_COLS * row] = (struct VGAChar) {
+        character: (uint8_t) c,
+        color: currentColor
+    };
+
+    col++;
+}
 
 void Log(const char* message)
 {
-    char* videoMemory = VGA_TEXT;
-
-    int i = 0;
-    while(message[i] != '\0')
+    for(int i = 0; 1; i++)
     {
-        videoMemory[i * 2] = message[i];
-        videoMemory[i * 2 + 1] = 0x07;
-        i++;
+        char c = (uint8_t)message[i];
+
+        if(c == '\0')
+            return;
+        
+        LogChar(c);
     }
 }
+
+void SetLogColor(uint8_t fgColor, uint8_t bgColor)
+{ currentColor = fgColor + (bgColor << 4); }
